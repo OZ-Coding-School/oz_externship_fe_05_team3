a1d879bfca584c0e7a1c54fb3493e2ae89af68c4
kimsowon <sowon836@gmail.com>
2026-01-02 15:14:18 +0900

    edit communitypage

--- src/pages/community-page/CommunityPage.tsx
+++ src/pages/community-page/CommunityPage.tsx
@@ -1,45 +1,19 @@
 import {
-  CategoryMenu,
-  CreateButton,
+  CategoryMenuSection,
   CommunityPagination,
-  Sort,
-  PostCard,
-  SearchFilter,
-  SearchInput,
+  PostCardSection,
+  SearchSection,
 } from '@/components/index';
 
-import { useCommunityPageData } from '@/hooks/index';
-import { Link } from 'react-router';
-import { type Post } from '@/types/index';
-import { ROUTES } from '@/routes';
-// import PostList from '@/components/community-page/PostList';
-
 function CommunityPage() {
-  const { posts } = useCommunityPageData();
-
   return (
-    <div className="mx-auto mt-25 mb-50 flex w-[960px] flex-col items-center px-4">
+    <div className="mx-auto mt-20 mb-50 flex w-full max-w-[960px] flex-col items-center px-4">
       <header className="mx-auto flex w-full flex-col gap-4">
-        <p className="text-[32px] font-bold">커뮤니티</p>
-        <section className="mb-10 flex items-center justify-between gap-4">
-          <div className="flex items-center gap-4">
-            <SearchFilter />
-            <SearchInput />
-          </div>
-          <CreateButton />
-        </section>
-        <section className="flex items-center justify-between gap-4">
-          <CategoryMenu />
-          <Sort />
-        </section>
-        <hr className="mb-5 w-full" />
+        <h1 className="text-[32px] font-bold">커뮤니티</h1>
+        <SearchSection />
+        <CategoryMenuSection />
       </header>
-      {posts?.map((post: Post) => (
-        <Link to={`${ROUTES.COMMUNITY}/${post.id}`} key={post.id}>
-          <PostCard postcard={post} />
-        </Link>
-      ))}
-      <hr className="mt-5 mb-10 w-full" />
+      <PostCardSection />
       <CommunityPagination />
     </div>
   );
