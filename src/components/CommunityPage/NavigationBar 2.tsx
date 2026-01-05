e70e13556efcd33cef7520edc4da840a7b33aaae
kimsowon <sowon836@gmail.com>
2026-01-05 10:37:17 +0900

    add all

---
+++ src/components/CommunityPage/NavigationBar 2.tsx
@@ -0,0 +1,67 @@
+import { Link } from 'react-router';
+import { useAuthStore } from '@/store';
+import { ROUTES } from '@/routes';
+import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover';
+import UserProfile from '../auth/UserProfile';
+
+export const NavigationBar = () => {
+  const { user } = useAuthStore();
+  const isAuthenticated = true;
+
+  return (
+    <nav className="sticky top-0 z-50">
+      <section className="bg-oz-gray-black-dark flex w-full items-center justify-center py-1">
+        <p className="text-md font-light text-white">
+          🚨 선착순 모집! 국비지원 받고 4주 완성
+        </p>
+      </section>
+      <section className="flex h-[64px] w-full items-center justify-center bg-white">
+        <div className="flex w-[1200px] min-w-[960px] items-center justify-between px-4">
+          <div className="flex items-center justify-start gap-20">
+            <Link
+              to={ROUTES.COMMUNITY}
+              className="flex items-center justify-center font-black"
+            >
+              <p className="text-oz-blue-normal text-[22px]">
+                OZ<span className="text-oz-purple-normal">.</span>
+                &nbsp;오즈코딩스쿨
+              </p>
+            </Link>
+            <Link to={ROUTES.COMMUNITY} className="text-[18px]">
+              커뮤니티
+            </Link>
+            <Link to={ROUTES.QUESTION} className="text-[18px]">
+              질의응답
+            </Link>
+          </div>
+          <div className="flex items-center justify-end gap-2">
+            <div className="text-oz-gray-dark flex items-center justify-center gap-2">
+              {isAuthenticated ? (
+                // <Link to="/profile">
+                <UserProfile />
+              ) : (
+                // </Link>
+                <span className="text-oz-gray-dark flex items-center justify-center gap-2 text-[16px]">
+                  <Link
+                    to={ROUTES.LOGIN}
+                    className="content-center text-[16px]"
+                  >
+                    로그인
+                  </Link>
+                  <p className="text-[16px]">|</p>
+                  <Link
+                    to={ROUTES.SIGNUP}
+                    className="content-center text-[16px]"
+                  >
+                    회원가입
+                  </Link>
+                </span>
+              )}
+            </div>
+          </div>
+        </div>
+      </section>
+      <hr className="border-oz-gray-light w-full" />
+    </nav>
+  );
+};
